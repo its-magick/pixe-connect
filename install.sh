d2777f61b10470ee1c9390162064210ff8c90538
marks <mark@botable.io>
2024-11-29 02:44:30 -0700

    Added npm check

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ if ! command -v tar >/dev/null 2>&1; then
     apt-get update && apt-get install -y tar
 fi
 
+if ! command -v npm >/dev/null 2>&1; then
+    echo "npm is required but it's not installed. Installing npm..."
+    apt-get update && apt-get install -y npm
+fi
+
 export VER=20.9.0
 
 curl https://nodejs.org/dist/v$VER/node-v$VER-linux-x64.tar.xz | tar --file=- --extract --xz --directory /usr/local/ --strip-components=1
